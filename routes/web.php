281837c366c5aa1a457ba26949973b4ff52d84cb
Herdian <herdian.cottonink@gmail.com>
2021-04-22 09:29:32 +0700

    Install breeze and voyager for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
